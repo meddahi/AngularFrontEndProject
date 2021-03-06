42b62817dd2281602a80cc547beedae7b7bec89d
meddahi <cahduran13043@gmail.com>
2021-03-07 00:51:04 +0300

    Refactoring

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -11,7 +11,7 @@ export class ProductComponent implements OnInit {
   product2: any = { productId: 2, productName: "Laptop", categoryId: 1, unitPrice: 5 }
   product3: any = { productId: 3, productName: "Mouse", categoryId: 1, unitPrice: 5 }
   product4: any = { productId: 4, productName: "Keyboard", categoryId: 1, unitPrice: 5 }
-  product5: any = { productId: 5, productName: "Camera", categoryId: 1, unitPrice: 5 }
+  product5: any = { productId: 5, productName: "Camera", categoryId: 1, unitPrice: 6 }
 
   products = [this.product1, this.product2, this.product3, this.product4, this.product5]
 
